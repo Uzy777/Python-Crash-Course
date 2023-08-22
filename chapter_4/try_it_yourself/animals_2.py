ecc6860d0d56605b0c05295ac420317ccef4d00f
Uzy777 <usmon.h@outlook.com>
2023-08-22 12:55:59 +0100

    Chapter 4 - In Progress

---
+++ chapter_4/try_it_yourself/animals_2.py
@@ -0,0 +1,5 @@
+animals = ["Lion", "Tiger", "Puma", "Lepoard"]
+for animal in animals:
+    print(f"A {animal} would would not make for a good pet!")
+
+print("\nThese animals above a wild and not meant to be kept as pets.")
