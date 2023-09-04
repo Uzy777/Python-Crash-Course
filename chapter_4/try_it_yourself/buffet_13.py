ff0772f79dc31c8549db0c78a2623b536f94f34d
Uzy777 <usmon.h@outlook.com>
2023-09-04 13:10:32 +0100

    Chapter 4 - Complete

---
+++ chapter_4/try_it_yourself/buffet_13.py
@@ -0,0 +1,10 @@
+print("The menu:")
+foods = ("pizza", "chips", "burger", "fish", "eggs")
+for food in foods:
+    print(food)
+
+
+print("\nUpdated version of the menu:")
+foods = ("pizza", "salad", "chicken", "fish", "eggs")
+for food in foods:
+    print(food)
