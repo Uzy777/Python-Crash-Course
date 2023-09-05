e4ae3f5529fae063f8686d867c91003fd7f44bab
Uzy777 <usmon.h@outlook.com>
2023-09-05 11:28:12 +0100

    Chapter 4 - Comple

---
+++ chapter_5/try_it_yourself/conditional_tests_1.py
@@ -0,0 +1,15 @@
+car = "subaru"
+print("Is car == subaru? I predict it will output True")
+print(car == "subaru")
+
+print("\nIs car == audi? I predict it will output False")
+print(car == "audi")
+
+#----------------#
+
+food = "cake"
+print("\nIs food == cake? I predict it will output True")
+print(food == "cake")
+
+print("\nIs food == chicken? I predict it will output False")
+print(food == "chicken")
