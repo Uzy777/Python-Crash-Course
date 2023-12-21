2d5f697f792b8b0e8a960901ce2ebb79e0e645c7
Uzy777 <usmon.h@outlook.com>
2023-12-21 11:16:15 +0000

    Chapter 8 - Complete

---
+++ chapter_8/try_it_yourself/printing_models_15.py
@@ -0,0 +1,7 @@
+# 8-15. Printing Models:    Put the fucntions for the example printing_models.py in a seperate file
+#                           called printing_functions.py. Write an import statement at the top of
+#                           printing_modules.py, and modify the file to use the imported functions.
+
+
+import printing_functions
+
