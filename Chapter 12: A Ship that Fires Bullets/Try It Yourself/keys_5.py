ae70f321e6620792e3eecf1872852f3b7c91e29f
Uzy777 <usmon.h@outlook.com>
2024-09-16 18:07:42 +0100

    Chapter 12 - Complete

--- Chapter 12: A Ship that Fires Bullets/Try It Yourself/keys_5.py
+++ Chapter 12: A Ship that Fires Bullets/Try It Yourself/keys_5.py
@@ -1,3 +1,24 @@
 # 12-5. Keys:       Make a Pygame file that creates an empty screen. In the event loop, print the 'event.key' attribute whenever a 'pygame.KEYDOWN'
 #                   event is detected. Run the program and press various keys to see how Pygame responds.
 
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # Close window event
+            running = False
+        elif event.type == pygame.KEYDOWN:  # Key press event
+            print(event.key)  # Print the key code
+
+    clock.tick(60)  # Limit FPS to 60
+
+# Properly quit Pygame
+pygame.quit()
+
---
+++ Chapter 12: A Ship that Fires Bullets/Try It Yourself/settings.py
@@ -0,0 +1,20 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialise the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        # self.bg_colour = (230, 230, 230)      # Grey
+        self.bg_colour = (0, 0, 0)              # Black
+
+        # Ship settings
+        self.ship_speed = 12
+
+        # Bullet settings
+        self.bullet_speed = 8.0
+        self.bullet_width = 15
+        self.bullet_height = 5
+        self.bullet_colour = (255, 255, 255)
+        self.bullets_allowed = 100000
