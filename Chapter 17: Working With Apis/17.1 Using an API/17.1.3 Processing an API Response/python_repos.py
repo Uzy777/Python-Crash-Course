bf3afbaa4761126a23a5407b4ee0dfe0182db994
Uzy777 <usmon.h@outlook.com>
2024-10-02 19:45:03 +0100

    Chapter 17 - In Progress

---
+++ Chapter 17: Working With Apis/17.1 Using an API/17.1.3 Processing an API Response/python_repos.py
@@ -0,0 +1,17 @@
+# Working With Apis - Using an API - Processing an API Response
+
+import requests
+
+# Make an API call and check the response.
+url = "https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars:>10000"
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Convert the response object to a dictionary.
+response_dict = r.json()
+
+# Process results.
+print(response_dict.keys())
