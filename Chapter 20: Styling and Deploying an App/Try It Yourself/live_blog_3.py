b6759440915625ac2edd1fb51260fbd25ed281d8
Uzy777 <usmon.h@outlook.com>
2024-10-16 18:34:04 +0100

    Chapter 20 - Complete

---
+++ Chapter 20: Styling and Deploying an App/Try It Yourself/live_blog_3.py
@@ -0,0 +1,4 @@
+# 20-3. Live Blog:      Deploy the Blog project you've been working on to Platform.sh. Make sure you set DEBUG to False,
+#                       so users don't see the full Django error pages when something goes wrong.
+
+# unable to perform because of root directory consisting of setup for learning_logs project already.
